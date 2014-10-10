46e6ac9152d8e9e40ecb1c45a066c83390a3f950
fjut1945 <>
2014-10-10 21:18:57 +0200

    Commited with ApexSQL Source Control

--- Tables/dbo.GitTest.sql
+++ Tables/dbo.GitTest.sql
@@ -4,7 +4,8 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[GitTest] (
 		[seeee]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[2eee]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[2eee]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[3eee]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[GitTest] SET (LOCK_ESCALATION = TABLE)
